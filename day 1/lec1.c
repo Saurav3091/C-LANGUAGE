355dea6576c86b6df0e2a0c00c785490dd203a9e
Saurav Patil <saurav7803@gmail.com>
2025-06-19 13:33:26 +0530

    Add files via upload

---
+++ day 1/lec1.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("name:saurav patil\n");
+	printf("age: 24\n");
+	printf("school: T&TV sarvajanik high school\n");
+	
+	
+	printf("- - - - -\n");    	  
+	printf("|       |\n");
+	printf("|       |\n");
+	printf("|       |\n");
+	printf("|       |\n");
+	printf("|       |\n");
+	printf("- - - - -\n");
+	
+	
+		printf(" - - -\n"); 
+		printf("|    |\n"); 
+		printf("N    |\n");
+		printf("A    |\n");
+		printf("M    |\n");
+		printf("E    |\n");
+		printf(" - - -\n"); 
+		
+	printf("*\n");
+	printf("* *\n");
+	printf("* * *\n");
+	printf("* * \n");
+	printf("*\n");
+	
+	
+	
+	
+	
+	
+	printf("*\n");
+	printf("*\n");
+	printf("*                     *\n");
+	printf("*                   *    *\n");
+	printf("*      *         *        *\n");
+	printf("*    *   *     *\n");
+	printf("*  *      *  *\n");
+	printf("* *        *\n");
+	printf("*\n");
+		
+		
+	
+	
+	
+}
+
+    
+    
