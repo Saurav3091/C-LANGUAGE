90261ed695a7636f86caa7568d4702d93a2edaed
Saurav Patil <saurav7803@gmail.com>
2025-06-19 13:43:27 +0530

    Add files via upload

---
+++ day 1/day lec1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("name: saurav patil\n");
+	printf("age: 24\n");
+	printf("mobile no: 123456789\n");
+	printf("course name: full stack development");
+}
