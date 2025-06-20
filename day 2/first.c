af4eb5e4736747071df7bc0a810c7b608d1383c6
Saurav Patil <saurav7803@gmail.com>
2025-06-20 15:16:13 +0530

    Add files via upload

---
+++ day 2/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+
+    inntf("(a-b)2\n");
+    print a = 5, b = 2;
+    pritf("a2 - 2ab + b2\n");
+    printf("%d - %d + %d\n", a * a, 2 * a * b, b * b);
+    printf("%d", (a * a) - (2 * a * b) + (b * b));
+}
