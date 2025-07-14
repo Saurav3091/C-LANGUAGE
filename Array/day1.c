d6846d8e1d0d603e1d911cc711b35d1755fb9559
Saurav Patil <saurav7803@gmail.com>
2025-07-14 16:27:09 +0530

    Add files via upload

---
+++ Array/day1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+// wap to print all elements of array
+{
+    int i;
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
